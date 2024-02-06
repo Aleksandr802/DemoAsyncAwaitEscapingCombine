d5742c26ff923f48f305f497ca433e30573fae86
Aleksandr802 <savs802@gmail.com>
2024-02-06 15:00:44 -0800

    Initial Commit

---
+++ DemoAsyncAwaitEscapingCombine/DemoAsyncAwaitEscapingCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAsyncAwaitEscapingCombineApp.swift
+//  DemoAsyncAwaitEscapingCombine
+//
+//  Created by Oleksandr Seminov on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAsyncAwaitEscapingCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
